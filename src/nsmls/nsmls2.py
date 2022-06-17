40f5c937eef6e92d763dec2c7343eff8a0c21508
d <NOREPLY@codeberg.org>
2022-06-17 20:20:39 +0200

    __init__.py

--- src/nsmls/nsmls2.py
+++ src/nsmls/nsmls2.py
@@ -7,6 +7,7 @@ import os
 import xdg.DesktopEntry #pyxdg  https://www.freedesktop.org/wiki/Software/pyxdg/
 #import xdg.IconTheme #pyxdg  https://www.freedesktop.org/wiki/Software/pyxdg/
 
+
 # NOTE: the default file MUST not have a entry commented out, other then in custom_clients.
 # NOTE: xdg specifications only have url for LINK entry it seems": https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html#recognized-keys
 # xdg.IconTheme.getIconPath(desktopEntry.getIcon    ()
@@ -28,121 +29,7 @@ class Client:
     known_client: bool = True
 
 
-
-# CONFIG 
-
-
-# Blocking clients is normally done by just commenting them out, with a '#'. There is also a general default blocked_clients list, but normally you wouldn't edit it.
-
-# Custom entries for custom or unknown clients. Please report if you think they should be known.
-custom_clients = [
-
-        #Client("mixbus", "https://harrisonconsoles.com", "digital audio workstation"),
-        # ("exec_name", "url", "info"),
-        Client("mamba", "https://github.com/brummer10/Mamba", "virtual midi keyboard"),
-
-
-        ]
-
-
-# List with clients that do have nsm support with 99% certainty, but don't have a (proper) desktop file and/ or are not maintained actively at the moment. 
-green_clients = [
-
-        Client("non-midi-mapper", "http://non.tuxfamily.org", "non-daw midi to osc mapper"),
-        Client("non-mixer", "http://non.tuxfamily.org", "non-daw mixer"),
-        Client("non-mixer-noui", "http://non.tuxfamily.org", "non-daw mixer"),
-        Client("non-sequencer", "http://non.tuxfamily.org/", "midi sequencer"),
-        Client("non-timeline", "http://non.tuxfamily.org", "non-daw audio recorder"),
-        Client("nsm-proxy", "http://non.tuxfamily.org", "launch tools with no nsm or gui"),
-        Client("jackpatch", "https://non.tuxfamily.org", "save jack connections"),
-        Client("zynaddsubfx", "https://github.com/zynaddsubfx", "synthesizer"),  # NOTE dificult one
-
-        ]
-
-
-# List with clients which are known to have NSM support and should have a proper *.desktop file. If not, report it to the developer and add the client to your custom_clients list, after you've
-# commented it out in this list in meantime.
-nsm_clients = [
-
-    Client("adljack", "https://github.com/jpcima/adljack", "opl3/opn2 synthesizer"),
-    Client("ams", "http://alsamodular.sourceforge.net", "modular synthesizer"),
-    Client("amsynth", "http://amsynth.github.io", "analog modelling synthesizer"),
-    Client("ardour", "https://ardour.org", "digital audio workstation"),
-    Client("ardour3", "https://ardour.org", "digital audio workstation"),
-    Client("ardour4", "https://ardour.org", "digital audio workstation"),
-    Client("ardour5", "https://ardour.org", "digital audio workstation"),
-    Client("ardour6", "https://ardour.org", "digital audio workstation"),
-    Client("ardour7", "https://ardour.org", "digital audio workstation"),
-    Client("carla-jack-multi", "https://github.com/falkTX/Carla", "plugin host multi"),
-    Client("carla-rack", "https://github.com/falkTX/Carla", "plugin host rack"),
-    Client("drumkv1_jack", "https://github.com/rncbc/drumkv1", "drumkit sampler"),
-    Client("fluajho", "https://laborejo.org", "soundfont player"),
-    Client("guitarix", "https://github.com/brummer10/guitarix", "virtual guitar amplifier"),
-    Client("hydrogen", "https://github.com/hydrogen-music/hydrogen", "drum machine"),
-    Client("jack_mixer", "https://rdio.space/jackmixer", "mixer"),
-    Client("laborejo", "https://laborejo.org", "music notation midi sequencing"),
-    Client("loop192", "https://github.com/jean-emmanuel/loop192", "midi looper"),
-    Client("luppp", "http://openavproductions.com/luppp", "live looper"),
-    Client("mamba", "https://github.com/brummer10/Mamba", "virtual midi keyboard"),
-    Client("mfp", "https://github.com/bgribble/mfp", "visual composing"),
-    Client("padthv1_jack", "https://github.com/rncbc/padthv1", "additive synthesizer"),
-    Client("patroneo", "https://laborejo.org", "midi sequencer"),
-    Client("petri-foo", "http://petri-foo.sourceforge.net", "sampler"),
-    Client("qmidiarp", "http://qmidiarp.sourceforge.net", "midi arpeggiator"),
-    Client("qtractor", "https://github.com/rncbc/qtractor", "digital audio workstation"),
-    Client("qseq66", "https://github.com/ahlstromcj/seq66", "midi sequencer"),
-    Client("radium", "http://users.notam02.no/~kjetism/radium", "tracker"),
-    Client("radium_compressor", "http://users.notam02.no/~kjetism/radium", "compressor"),
-    Client("samplv1_jack", "https://github.com/rncbc/samplv1", "sampler synthesizer"),
-    Client("seq192", "https://github.com/jean-emmanuel/seq192", "midi sequencer"),
-    Client("shuriken", "https://rock-hopper.github.io/shuriken", "beat slicer"),
-    Client("synthpod_jack", "https://open-music-kontrollers.ch/lv2", "lv2 plugin container"), # FIXME
-    Client("synthv1_jack", "https://github.com/rncbc/synthv1", "substractive synthesizer"),
-    Client("tembro", "https://laborejo.org/tembro/", "virtual instrument samples"),
-    Client("xtuner", "https://github.com/brummer10/XTuner", "instrument tuner"),
-    Client("zita-at1", "https://github.com/royvegard/zita-at1", "autotuner (fork)"),
-    Client("zita-rev1", "https://github.com/royvegard/zita-rev1", "reverb (fork)"),
-
-
-        ]
-
-
-# Normally you wouldn't edit this list, unless you know what you're doing.
-blocked_clients = (
-
-        # "non-midi-mapper", 
-        # "non-mixer-noui"
-        #carla-patchbay https://kx.studio/Clientlications:Carla jack patchbay
-
-        "nsmd",
-        "non-daw", 
-        "carla", 
-        "agordejo", 
-        "adljack", 
-        "agordejo.bin",
-        # We block raysession specific tools to keep things simple and clean.
-        "ray_control",
-        "ray-jack_checker_daemon",
-        "ray-pulse2jack",
-        "ray-daemon",
-        "ray-jack_config_script",
-        "raysession",
-        "ray_git",
-        "ray-proxy",
-        "ray-jackpatch",
-        # We block Agordejo specific tools to keep things simple and clean.
-        "nsm-data",          
-        )
-
-
-
-xdg_paths = (
-
-        Path("/usr/share/applications"),
-        Path("/usr/local/share/applications"),
-        Path(Path.home(), ".local/share/applications"),
-
-        )
+from config.nsmlsconfig import * 
 
 
 joinedlist = custom_clients + green_clients
