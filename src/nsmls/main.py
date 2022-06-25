a357a404f1ff116a3ca21729ee822e9f8aa09477
d <NOREPLY@codeberg.org>
2022-06-25 16:15:09 +0200

    copyright

--- src/nsmls/main.py
+++ src/nsmls/main.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
+
+"""
+Copyright 2022, D. H., The Netherlands.
+
+This file is part of nsmls.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License version 2 as published by the Free Software Foundation.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+Boston, MA  02110-1301, USA.
+
+Or see <http://www.gnu.org/licenses/>
+
+"""
+
 from shutil import which
 
 import argparse
