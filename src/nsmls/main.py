0789a4c672af88be9cc9fb39a9de7213c56a97a6
d <NOREPLY@codeberg.org>
2022-06-26 20:39:36 +0200

    workaround

--- src/nsmls/main.py
+++ src/nsmls/main.py
@@ -67,9 +67,9 @@ def print_all_info(args):
         elif client.installed and not client.blocked:
             print(f"\033[2m{client.exec_name:<18} {client.info} \033[2m{client.url}\033[m")
         elif client.installed and client.blocked:
-            print(f"\033[2;3m{client.exec_name:<18} {client.info} \033[2m{client.url}\033[m")
             #print(\033[9;2mclient.exec_name\033[m\:<18{client.info} \033[2m{client.url}\033[m"  )  # FIXME
-            #print(f"\033[2m{[client.exec_name]:<18} {client.info} \033[2m{client.url}\033[m")
+            #print(f"\033[9;2m{client.exec_name:<18} {client.info} \033[2m{client.url}\033[m")  # FIXME
+            print(f"\033[9;2m{client.exec_name}\033[m")
 
     print()
     for client in args.nsm_clients:
