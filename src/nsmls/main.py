95f06b5287282e21aef1a9df812d16b7b39741eb
d <NOREPLY@codeberg.org>
2022-06-27 11:10:59 +0200

    27 magic number

--- src/nsmls/main.py
+++ src/nsmls/main.py
@@ -67,7 +67,7 @@ def print_all_info(args):
         elif client.installed and client.blocked:
             exec_name = f"\033[9;2m{client.exec_name}\033[m" 
             info = f"\033[9;2m{client.info} {client.url}\033[m" 
-            print("%-*s %s" % (27, exec_name, info))
+            print("%-*s %s" % (27, exec_name, info)) # NOTE: 27 is a bit magic, why not 18?
     print()
     for client in args.nsm_clients:
         if not client.installed and not client.nsmls and not client.blocked:
