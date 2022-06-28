ed3a931cc8d94a5eb8b4cbb78f0a0dab9dc0e690
d <NOREPLY@codeberg.org>
2022-06-28 15:22:53 +0200

    edit config

--- src/config/config.def.py
+++ src/config/config.def.py
@@ -7,31 +7,32 @@ from libnsmls.nsmls_dataclass import Client
 # USER CONFIG 
 
 
-# Custom (black)star entries for own apps or apps which don't have a good xdg *.desktop file (tell the developer). 
-# This overrides entries in the global blocked_clients list.
-user_star_clients = [
-    
+# Custom user entries. This overrides the global blocked_clients list. We need the executable name, with quotes and a comma.
+# For a description and url, the entry should be listed in the list nsm_clients. 
 
+user_star_clients = [
+
+        # example1,
+        # example2,
 
         ]
 
 
-# Blocked clients by the user. NOTE: Syntax is different. We just need the executable name, with quotes and a comma.
+# Blocked clients by the user.
+
 user_blocked_clients = [
 
-        # "example",
-        "non-sequencer",
-        "ams",
+        # "example1",
+        # "example2",
+
          ]
 
 
 # END USER CONFIG
 
 
-
-
-# List with clients which are known to have NSM support and should have a proper *.desktop file. If not, report it to the developer and copy the Client to your user_star_clients list in meantime.
-# Normally a user shouldn't edit this (other then copying a entry).
+# List with clients which are known to have NSM support. They'll be listed if they have a xdg *.desktop file with a 'X-NSM-Exec' key.
+# Normally a user should only edit this when a custom client in user_star_clients needs a description and a url.
 
 nsm_clients = [
         
@@ -49,6 +50,7 @@ nsm_clients = [
         Client("fluajho", "soundfont player", "https://laborejo.org"),
         Client("guitarix", "guitar amplifier", "https://github.com/brummer10/guitarix"),
         Client("hydrogen", "drum machine", "https://github.com/hydrogen-music/hydrogen"),
+        Client("jackpatch", "save jack connections", "https://non.tuxfamily.org"),
         Client("jack_mixer", "mixer", "https://rdio.space/jackmixer"),
         Client("laborejo", "music notation midi sequencing", "https://laborejo.org"),
         Client("loop192", "midi looper", "https://github.com/jean-emmanuel/loop192"),
@@ -56,6 +58,12 @@ nsm_clients = [
         Client("mamba", "virtual midi keyboard", "https://github.com/brummer10/Mamba"),
         Client("mixbus", "digital audio workstation", "https://harrisonconsoles.com"),  # FIXME
         Client("mfp", "visual composing", "https://github.com/bgribble/mfp"),
+        Client("non-midi-mapper", "non-daw midi to osc mapper", "http://non.tuxfamily.org"),
+        Client("non-mixer", "non-daw mixer", "http://non.tuxfamily.org"),
+        Client("non-mixer-noui", "non-daw mixer", "http://non.tuxfamily.org"),
+        Client("non-sequencer", "midi sequencer", "http://non.tuxfamily.org/"),
+        Client("non-timeline", "non-daw audio recorder", "http://non.tuxfamily.org"),
+        Client("nsm-proxy", "launch tools without nsm or gui", "http://non.tuxfamily.org"),
         Client("padthv1_jack", "additive synthesizer", "https://github.com/rncbc/padthv1"),
         Client("patroneo", "midi sequencer", "https://laborejo.org"),
         Client("petri-foo", "sampler", "http://petri-foo.sourceforge.net"),
@@ -75,17 +83,7 @@ nsm_clients = [
         Client("xtuner", "instrument tuner", "https://github.com/brummer10/XTuner"),
         Client("zita-at1", "autotune (nsm patch)", "https://github.com/royvegard/zita-at1"),
         Client("zita-rev1", "reverb (nsm patch)", "https://github.com/royvegard/zita-rev1"),
-        Client("non-midi-mapper", "non-daw midi to osc mapper", "http://non.tuxfamily.org"),
-        Client("non-mixer", "non-daw mixer", "http://non.tuxfamily.org"),
-        Client("non-mixer-noui", "non-daw mixer", "http://non.tuxfamily.org"),
-        Client("non-sequencer", "midi sequencer", "http://non.tuxfamily.org/"),
-        Client("non-timeline", "non-daw audio recorder", "http://non.tuxfamily.org"),
-        Client("nsm-proxy", "launch tools without nsm or gui", "http://non.tuxfamily.org"),
-        Client("jackpatch", "save jack connections", "https://non.tuxfamily.org"),
         Client("zynaddsubfx", "synthesizer", "https://github.com/zynaddsubfx"),
-
-
-
 
 
         ]
